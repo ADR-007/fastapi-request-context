f656b0f4c79b23cc8ae0f6493c484ec332c9b901
Adrian Dankiv <adr-007@ukr.net>
2026-01-25 22:22:10 +0100

    chore: remove unused files

--- .history/fastapi_request_context/formatters/__init___20251128202921.py
+++
@@ -1,9 +0,0 @@
-"""Logging formatters with context injection."""
-
-from fastapi_request_context.formatters.json import JsonContextFormatter
-from fastapi_request_context.formatters.local import LocalContextFormatter
-
-__all__ = [
-    "JsonContextFormatter",
-    "LocalContextFormatter",
-]
